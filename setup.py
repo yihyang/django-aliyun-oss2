7077d9d6deb90864879d08c4d126f75036322253
Yih Yang <yyngo788n88@hotmail.com>
2024-06-03 10:15:07 +0800

    Added Python 3.11 support in setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
